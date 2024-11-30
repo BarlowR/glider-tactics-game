76d05c2821f006163f4aee91b2f7dc03abc89f88
rbarlow <rbarlow@reliable.co>
2024-11-29 19:56:51 -0800

    Switch to GA plugin

--- soaring-game/vite.config.js
+++ soaring-game/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-95MKMKP20C',
+      },
+    })
+  ],
   base: "/glider-tactics-game/"
 })
